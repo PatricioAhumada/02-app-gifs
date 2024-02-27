076be2fa10aeed2e6178f47bd5947c86e7cd6ea4
Patricio Ahumada <patricio.ahumada@gmail.com>
2024-02-27 01:23:25 -0300

    inicio push

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
+import { Gif } from '../../../gifs/interfaces/gifs.interfaces';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+  constructor(private gifsService : GifsService){}
+
+  get tags(){
+    return this.gifsService.tagsHistory;
+  }
+
+  searchTag( tag: string){
+    this.gifsService.searchTag(tag);
+  }
+
+
+}
